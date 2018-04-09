15d6b63d35fd6be82ce632d59b76947ed56ffdfa
Luiz Miguel <creepy_luiz@hotmail.com>
2018-04-09 17:24:17 -0300

    Update config.php

--- Produto/config.php
+++ Produto/config.php
@@ -16,7 +16,7 @@ define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 /** caminho absoluto para a pasta do sistema **/
 if ( !defined('ABSPATH') )
-	define('ABSPATH', ROOT.'');
+	define('ABSPATH', ROOT);
 	
 /** caminho no server para o sistema **/
 if ( !defined('BASEURL') )
